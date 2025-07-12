6cc1d10088a5ea9552a6f6940ec05bf3fe6b595a
hedaunaval03 <hedau.naval03@gmail.com>
2025-07-12 21:05:47 +0530

    Revert "This is a pvtbranch2 to be reverted change"
    This reverts commit cf862b71d344c5e663eb227e5e47aeb9304f8fc8.

--- src/main/java/artifactpackage1/Demo.java
+++ src/main/java/artifactpackage1/Demo.java
@@ -5,6 +5,6 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("qwertyu");
 		System.out.println("Chnage from github");
-//		"This is a pvtbranch2 to be reverted change"
+//		"This is a pvtbranch2 change"
 		}
 }
