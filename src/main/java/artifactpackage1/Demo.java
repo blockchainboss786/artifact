604d1738526bea3d669dfdcb51c547374bf9a712
blockchainboss786 <blockchainboss786@gmail.com>
2025-07-12 11:24:22 +0530

    conflict creationDemo.java

--- src/main/java/artifactpackage1/Demo.java
+++ src/main/java/artifactpackage1/Demo.java
@@ -4,6 +4,7 @@ public class Demo {
  
 	public static void main(String[] args) {
 		System.out.println("qwertyu");
-		System.out.println("Chnage for mergeconflict branch");
+		System.out.println("Chnage for mergeconflict creation branch");
+		System.out.println("Chnage for conflict creation branch");
 	}
 }
