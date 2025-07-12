c3405fb8756362156f0a7841890fc3af6d6d2e3a
blockchainboss786 <blockchainboss786@gmail.com>
2025-07-12 20:08:38 +0530

    changefromGHDemo.java

--- src/main/java/artifactpackage1/Demo.java
+++ src/main/java/artifactpackage1/Demo.java
@@ -4,8 +4,7 @@ public class Demo {
  
 	public static void main(String[] args) {
 		System.out.println("qwertyu");
-		System.out.println("Chnage from github branch");
-		System.out.println("Chnage for conflict creation github branch");
-		System.out.println("change from eclipse");
+		System.out.println("Chnage from github");
+		
 		}
 }
