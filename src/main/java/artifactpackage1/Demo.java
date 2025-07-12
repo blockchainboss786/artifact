71b6bea27aa66735ebcbdd63c46075ed3e83866d
hedaunaval03 <hedau.naval03@gmail.com>
2025-07-12 11:59:40 +0530

    conflict changes

--- src/main/java/artifactpackage1/Demo.java
+++ src/main/java/artifactpackage1/Demo.java
@@ -6,5 +6,6 @@ public class Demo {
 		System.out.println("qwertyu");
 		System.out.println("Chnage from github branch");
 		System.out.println("Chnage for conflict creation github branch");
-	}
+		System.out.println("change from eclipse");
+		}
 }
