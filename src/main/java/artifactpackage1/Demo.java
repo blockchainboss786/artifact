287b31c441fe596c5f9b2bf24bc254569b6fa565
hedaunaval03 <hedau.naval03@gmail.com>
2025-07-12 20:56:35 +0530

    MC change in pvt branch to merge to main

--- src/main/java/artifactpackage1/Demo.java
+++ src/main/java/artifactpackage1/Demo.java
@@ -5,6 +5,6 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("qwertyu");
 		System.out.println("Chnage from github");
-		
+//		"This is a pvtbranch change"
 		}
 }
